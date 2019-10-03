bb7ca889726d351c90a06ff9c664015c76c40153
Marcin Zapadka <marcin.zapadka@softwarehut.com>
2019-10-03 08:36:26 +0200

    Create logic project and add base repository interface and implementation

---
+++ ProductsApp.DataAccess/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsApp.Logic.Repositories;
+using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApp.DataAccess
+{
+    public abstract class Repository<T> : IRepository<T> where T : BaseModel
+    {
+        private readonly Lazy<DataContext> _db;
+        protected DataContext Db => _db.Value;
+
+        protected Repository(Lazy<DataContext> db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(T model)
+        {
+            await Db.Set<T>().AddAsync(model);
+        }
+
+        public void Delete(T model)
+        {
+            Db.Set<T>().Remove(model);
+        }
+
+        public async Task<IEnumerable<T>> GetAllActive()
+        {
+            return await Db.Set<T>().Where(m => m.IsActive)
+                .ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await Db.Set<T>().FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task SaveChanges()
+        {
+            await Db.SaveChangesAsync();
+        }
+    }
+}
---
+++ ProductsApp.Logic/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductsApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProductsApp.Logic.Repositories
+{
+    public interface IRepository<T> where T : BaseModel
+    {
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> GetAllActive();
+        Task Add(T model);
+        void Delete(T model);
+        Task SaveChanges();
+    }
+}
