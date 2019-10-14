e7182374c8904acc8d7d6967501e4fa6cb423c52
Marcin Zapadka <marcin.zapadka@softwarehut.com>
2019-10-14 12:00:46 +0200

    Configure one to many relationship between category and product entities

--- ProductsApp.Models/Category.cs
+++ ProductsApp.Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace ProductsApp.Models
+﻿using System.Collections.Generic;
+
+namespace ProductsApp.Models
 {
     public class Category : BaseModel
     {
         public string Name { get; set; }
+        public ICollection<Product> Products { get; } = new List<Product>();
     }
 }
