0b823da2c541b4e176503180b4fd51697840c505
Marcin Zapadka <marcin.zapadka@softwarehut.com>
2019-10-15 09:50:18 +0200

    Include category in product update method

--- ProductsApp.WebApi/Mappers/ProductProfile.cs
+++ ProductsApp.WebApi/Mappers/ProductProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ProductsApp.Logic.Repositories;
 using ProductsApp.Models;
 using ProductsApp.WebApi.Dto;
 
@@ -9,9 +10,27 @@ namespace ProductsApp.WebApi.Mappers
         public ProductProfile()
         {
             CreateMap<ProductForCreationDto, Product>();
-            CreateMap<Product, ProductDto>()                
-                .ForMember(m => m.CategoryName, 
+            CreateMap<Product, ProductDto>()
+                .ForMember(m => m.CategoryName,
                     opt => opt.MapFrom(p => p.Category.Name));
+            CreateMap<ProductForCreationDto, ProductDto>()
+                .ForMember(m => m.CategoryName,
+                    opt => opt.MapFrom<CategoryResolver>());
+        }
+    }
+
+    public class CategoryResolver : IValueResolver<ProductForCreationDto, ProductDto, string>
+    {
+        private ICategoryRepository _repository;
+
+        public CategoryResolver(ICategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public string Resolve(ProductForCreationDto source, ProductDto destination, string destMember, ResolutionContext context)
+        {
+            return _repository.GetById(source.CategoryId).Result.Name;
         }
     }
 }
