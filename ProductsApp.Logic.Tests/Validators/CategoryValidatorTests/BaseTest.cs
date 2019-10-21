d1394e328210d34e7bc2b0505be0b1df8eada32f
Marcin Zapadka <marcin.zapadka@softwarehut.com>
2019-10-21 11:28:12 +0200

    Add validation tests for categories

---
+++ ProductsApp.Logic.Tests/Validators/CategoryValidatorTests/BaseTest.cs
@@ -0,0 +1,12 @@
+﻿using ProductsApp.Logic.Validators;
+
+namespace ProductsApp.Logic.Tests.Validators.CategoryValidatorTests
+{
+    public class BaseTest
+    {
+        protected virtual CategoryValidator Create()
+        {
+            return new CategoryValidator();
+        }
+    }
+}
---
+++ ProductsApp.Logic.Tests/Validators/CategoryValidatorTests/NameTest.cs
@@ -0,0 +1,53 @@
+﻿using FizzWare.NBuilder;
+using FluentValidation.TestHelper;
+using ProductsApp.Logic.Validators;
+using Xunit;
+
+namespace ProductsApp.Logic.Tests.Validators.CategoryValidatorTests
+{
+    public class NameTest : BaseTest
+    {
+        protected string Name { get; set; }
+
+        protected override CategoryValidator Create()
+        {
+            var validator = base.Create();
+            CorrectFlow();
+            return validator;
+        }
+
+        private void CorrectFlow()
+        {
+            Name = "Domyślna nazwa kategorii";
+        }
+
+        [Fact]
+        public void Return_Success()
+        {
+            //Arrange
+            var validator = Create();
+            //Assert
+            validator.ShouldNotHaveValidationErrorFor(c => c.Name, Name);
+        }
+
+        [Fact]
+        public void Return_Error_When_Name_Is_Null()
+        {
+            //Arrange
+            var validator = Create();
+            Name = null;
+            //Assert
+            validator.ShouldHaveValidationErrorFor(c => c.Name, Name);
+        }
+
+        [Fact]
+        public void Return_Error_When_Name_Length_Is_Invalid()
+        {
+            //Arrange
+            var validator = Create();
+            Name = "aa";
+            //Assert
+            validator.ShouldHaveValidationErrorFor(c => c.Name, Name);
+        }
+    }
+}
