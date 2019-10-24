7df5d26b68163ee9b9e5887f48748fbd904c6fd8
Marcin Zapadka <marcin.zapadka@softwarehut.com>
2019-10-24 13:56:16 +0200

    Create project for web api tests and add controller based extensions

---
+++ ProductsApp.WebApi.Tests/Infrastructure/ControllerBaseExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public static class ControllerBaseExtensions
+    {
+        public static ControllerBaseAssertion Should(this ControllerBase instance)
+        {
+            return new ControllerBaseAssertion(instance);
+        }
+    }
+
+    public class ControllerBaseAssertion : ReferenceTypeAssertions<ControllerBase, ControllerBaseAssertion>
+    {
+        public ControllerBaseAssertion(ControllerBase controller)
+        {
+            Subject = controller;
+        }
+
+        protected override string Identifier => "Controller";
+
+        public ControllerBaseAssertion HasError(string property,
+            string message,
+            string because = "",
+            params object[] becauseArgs)
+        {
+            var state = Subject.ModelState.FirstOrDefault(m => m.Key == property);
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(state.Key == property)
+                .FailWith($"The controller should have error for property: {property}");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(state.Value.Errors.Any(e => e.ErrorMessage == message))
+                .FailWith($"The controller should have error with message: {message}");
+
+            return this;
+        }
+    }
+}
