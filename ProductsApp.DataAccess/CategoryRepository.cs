6f73493a4dcc32803a50254a731b77eabaac4a20
Marcin Zapadka <marcin.zapadka@softwarehut.com>
2019-11-05 08:01:00 +0100

    Remove all products related to a category when removing that category using EntityFramework Plus batch update method

--- ProductsApp.DataAccess/CategoryRepository.cs
+++ ProductsApp.DataAccess/CategoryRepository.cs
@@ -1,5 +1,7 @@
-﻿using ProductsApp.Logic.Repositories;
+﻿using System.Linq;
+using ProductsApp.Logic.Repositories;
 using ProductsApp.Models;
+using Z.EntityFramework.Plus;
 
 namespace ProductsApp.DataAccess
 {
@@ -7,6 +9,14 @@ namespace ProductsApp.DataAccess
     {
         public CategoryRepository(DataContext db) : base(db)
         {
+        }
+
+        public override void Delete(Category category)
+        {
+            category.IsActive = false;
+
+            _db.Products.Where(p => p.CategoryId == category.Id)
+                .Update(p => new Product() { IsActive = false });
         }
     }
 }
--- ProductsApp.DataAccess/ProductRepository.cs
+++ ProductsApp.DataAccess/ProductRepository.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ProductsApp.Logic.Repositories;
+﻿using ProductsApp.Logic.Repositories;
 using ProductsApp.Models;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProductsApp.DataAccess
 {
